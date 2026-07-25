85366987bcb2e8ad866adee8c7b8f87514d5776d
elebras1 <erwan.lebras27@gmail.com>
2026-07-25 14:04:11 +0200

    update flecs contants

--- src/main/java/io/github/elebras1/flecs/util/Flecs.java
+++ src/main/java/io/github/elebras1/flecs/util/Flecs.java
@@ -12,18 +12,49 @@ public final class Flecs {
     public static final long FlecsCore = flecs_h.EcsFlecsCore();
     public static final long World = flecs_h.EcsWorld();
 
-    public static final long Prefab = flecs_h.EcsPrefab();
-    public static final long SlotOf = flecs_h.EcsSlotOf();
-    public static final long Disabled = flecs_h.EcsDisabled();
-    public static final long NotQueryable = flecs_h.EcsNotQueryable();
+    public static final long Wildcard = flecs_h.EcsWildcard();
+    public static final long Any = flecs_h.EcsAny();
+    public static final long This = flecs_h.EcsThis();
+    public static final long Variable = flecs_h.EcsVariable();
+    public static final long Transitive = flecs_h.EcsTransitive();
+    public static final long Reflexive = flecs_h.EcsReflexive();
+    public static final long Final = flecs_h.EcsFinal();
+    public static final long Inheritable = flecs_h.EcsInheritable();
+    public static final long Exclusive = flecs_h.EcsExclusive();
+    public static final long Acyclic = flecs_h.EcsAcyclic();
+    public static final long Traversable = flecs_h.EcsTraversable();
+    public static final long Symmetric = flecs_h.EcsSymmetric();
+    public static final long With = flecs_h.EcsWith();
+    public static final long OneOf = flecs_h.EcsOneOf();
+    public static final long Cascade = flecs_h.EcsCascade();
+    public static final long Trait = flecs_h.EcsTrait();
+    public static final long Relationship = flecs_h.EcsRelationship();
+    public static final long Target = flecs_h.EcsTarget();
+    public static final long PairIsTag = flecs_h.EcsPairIsTag();
+
+    public static final long CanToggle = flecs_h.EcsCanToggle();
+    public static final long DontFragment = flecs_h.EcsDontFragment();
+    public static final long Sparse = flecs_h.EcsSparse();
+
+    public static final long OnInstantiate = flecs_h.EcsOnInstantiate();
+    public static final long Override = flecs_h.EcsOverride();
+    public static final long Inherit = flecs_h.EcsInherit();
+    public static final long DontInherit = flecs_h.EcsDontInherit();
 
     public static final long ChildOf = flecs_h.EcsChildOf();
     public static final long IsA = flecs_h.EcsIsA();
     public static final long DependsOn = flecs_h.EcsDependsOn();
+    public static final long SlotOf = flecs_h.EcsSlotOf();
+    public static final long OrderedChildren = flecs_h.EcsOrderedChildren();
+    public static final long Module = flecs_h.EcsModule();
+
+    public static final long Prefab = flecs_h.EcsPrefab();
+    public static final long Disabled = flecs_h.EcsDisabled();
+    public static final long NotQueryable = flecs_h.EcsNotQueryable();
+    public static final long Singleton = flecs_h.EcsSingleton();
+
     public static final long OnDelete = flecs_h.EcsOnDelete();
     public static final long OnDeleteTarget = flecs_h.EcsOnDeleteTarget();
-    public static final long Cascade = flecs_h.EcsCascade();
-
     public static final long Remove = flecs_h.EcsRemove();
     public static final long Delete = flecs_h.EcsDelete();
     public static final long Panic = flecs_h.EcsPanic();
@@ -52,22 +83,6 @@ public final class Flecs {
     public static final long OnStore = flecs_h.EcsOnStore();
     public static final long PostFrame = flecs_h.EcsPostFrame();
 
-    public static final long Wildcard = flecs_h.EcsWildcard();
-    public static final long Any = flecs_h.EcsAny();
-    public static final long This = flecs_h.EcsThis();
-    public static final long Variable = flecs_h.EcsVariable();
-    public static final long Transitive = flecs_h.EcsTransitive();
-    public static final long Reflexive = flecs_h.EcsReflexive();
-    public static final long Final = flecs_h.EcsFinal();
-    public static final long DontInherit = flecs_h.EcsDontInherit();
-    public static final long Exclusive = flecs_h.EcsExclusive();
-    public static final long Acyclic = flecs_h.EcsAcyclic();
-    public static final long Traversable = flecs_h.EcsTraversable();
-    public static final long Symmetric = flecs_h.EcsSymmetric();
-    public static final long With = flecs_h.EcsWith();
-    public static final long OneOf = flecs_h.EcsOneOf();
-    public static final long CanToggle = flecs_h.EcsCanToggle();
-
     public static final int QueryCacheAuto = flecs_h.EcsQueryCacheAuto();
     public static final int QueryMatchEmptyTables = flecs_h.EcsQueryMatchEmptyTables();
     public static final int In = flecs_h.EcsIn();
@@ -95,6 +110,4 @@ public final class Flecs {
     public static final long Period1m = flecs_h.EcsPeriod1m();
     public static final long Period1h = flecs_h.EcsPeriod1h();
     public static final long Period1d = flecs_h.EcsPeriod1d();
-
 }
-
